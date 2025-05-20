8496624989c071ef7b4fff73c260195ec0d3bb58
dileepkumar1 <146738219+droots1@users.noreply.github.com>
2025-05-20 22:14:32 +0530

    theme added in project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,49 @@
-import React from "react";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { Button } from "@/components/ui/button";
-import { Text } from "@/components/ui/text";
-import { View } from "react-native";
+import { SafeAreaView } from "@/components/ui/safe-area-view";
+import { useFonts } from "expo-font";
+import { Slot } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "@/global.css";
+import { Button, ButtonText } from "@/components/ui/button";
+import { AuthProvider } from "@/contexts/auth-context";
+import { LocalizationProvider } from "@/i18n/localization-context";
+	
+function Example() {
+  return (
+    <Button size="md" variant="solid" action="primary" >
+          <ButtonText>Hello World!</ButtonText>
+        </Button>
+  );
+}
 
-export default function TestLayout() {
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <GluestackUIProvider mode="light">
-      <View className="flex-1 bg-background-0">
-        <Button variant="solid" className="m-4">
-          <Text className="text-typography-0">Test Button</Text>
-        </Button>
-      </View>
+      <LocalizationProvider>
+      <AuthProvider>
+       <SafeAreaView style={{ flex: 1 }}>
+        <Slot />
+      </SafeAreaView>
+      </AuthProvider>
+      </LocalizationProvider>
     </GluestackUIProvider>
   );
 }
